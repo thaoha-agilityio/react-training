c39576360a00dda0ef55ff489dac755c0a9c9e79
Thao Y Ha <thao.ha@asnet.com.vn>
2022-10-31 16:20:35 +0700

    update styled for component

--- practice-one/src/components/Button/index.stories.tsx
+++ practice-one/src/components/Button/index.stories.tsx
@@ -2,24 +2,61 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Button from '.';
 
 export default {
-  title: 'Button',
+  title: 'Components/Button',
   component: Button,
-  // argTypes: {
-  //   primary: {
-  //     defaultValue: true,
-  //   },
-  // },
+  argTypes: {
+    variant: {
+      defaultValue: 'primary',
+      options: ['primary', 'secondary', 'tertiary'],
+      control: { type: 'radio' },
+    },
+    size: {
+      defaultValue: 'small',
+      options: ['small', 'medium', 'large'],
+      control: { type: 'radio' },
+    },
+    children: {
+      defaultValue: 'click',
+      control: { type: 'text' },
+    },
+  },
+  parameters: {
+    actions: {
+      handles: ['click'],
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args}>Click</Button>;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-const ButtonPrimary = Template.bind({});
+export const Primary = Template.bind({});
 
-ButtonPrimary.args = {
-  ...ButtonPrimary.args,
-  size: 'medium',
-  children: 'Add User',
-  primary: true,
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  ...Secondary.args,
+  variant: 'secondary',
 };
 
-export { ButtonPrimary };
+export const Tertiary = Template.bind({});
+
+Tertiary.args = {
+  ...Tertiary.args,
+  variant: 'tertiary',
+};
+
+export const Small = Template.bind({});
+
+export const Medium = Template.bind({});
+
+Medium.args = {
+  ...Medium.args,
+  size: 'medium',
+};
+
+export const Large = Template.bind({});
+
+Large.args = {
+  ...Large.args,
+  size: 'large',
+};
