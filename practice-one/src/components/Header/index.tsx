fb78bcf719397d943b2a4b54b89da9730dc579a1
Thao Y Ha <thao.ha@asnet.com.vn>
2022-11-04 14:26:22 +0700

    update images name

--- practice-one/src/components/Header/index.tsx
+++ practice-one/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from '../../assets/images/logo.jpg';
-import notification from '../../assets/images/notificationIcon.png';
-import dark from '../../assets/images/darkIcon.png';
+import logo from '../../assets/images/logo.png';
+import notification from '../../assets/images/notificationIcon.jpg';
+import dark from '../../assets/images/darkIcon.jpg';
 import './index.css';
 
 class Header extends React.PureComponent {
