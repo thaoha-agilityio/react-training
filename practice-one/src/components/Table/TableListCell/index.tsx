008882fbf91dc165d0c9626428f513f831863cdd
Thao Y Ha <thao.ha@asnet.com.vn>
2022-11-07 15:35:39 +0700

    update name and add type for function

--- practice-one/src/components/Table/TableListCell/index.tsx
+++ practice-one/src/components/Table/TableListCell/index.tsx
@@ -7,35 +7,33 @@ interface IProps {
   hasLoadMore: boolean;
 }
 
-type ProjectRender = Partial<IProps>;
-
-class TableListCell extends React.Component<ProjectRender, IProps> {
+const TableListCell = ({ listProject, hasLoadMore }: IProps): React.ReactElement => {
   // Render data table cell
-  renderCell = <T, K extends keyof T, V extends keyof T>(data: T[] | undefined, key: K, id: V) => {
+  const renderCell = <T, K extends keyof T, V extends keyof T>(
+    data: T[] | undefined,
+    key: K,
+    id: V
+  ) => {
     //Check data empty
     if (!data) {
       return '';
     }
 
     // Get first 2 element or all element
-    const loadCount = this.props.hasLoadMore ? data.length : 2;
+    const loadCount = hasLoadMore ? data.length : 2;
 
     return data
       .slice(0, loadCount)
       .map((item: T) => <p key={item[id] as string}>{item[key] as string}</p>);
   };
 
-  render(): React.ReactNode {
-    const { listProject } = this.props;
-
-    return (
-      <>
-        <td>{this.renderCell(listProject, 'role', 'id')}</td>
-        <td>{this.renderCell(listProject, 'projectName', 'id')}</td>
-        <td>{this.renderCell(listProject, 'status', 'id')}</td>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <td>{renderCell(listProject, 'role', 'id')}</td>
+      <td>{renderCell(listProject, 'projectName', 'id')}</td>
+      <td>{renderCell(listProject, 'status', 'id')}</td>
+    </>
+  );
+};
 
 export default TableListCell;
--- practice-one/src/components/Table/TableRow/index.tsx
+++ practice-one/src/components/Table/TableRow/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Avatar from '../../Avatar';
-import { IUser } from '../../../types/IUser';
+import { IProject, IUser } from '../../../types/IUser';
 import moreVert from '../../../assets/images/moreVertIcon.jpg';
 import RowMore from '../RowMore';
 import TableListCell from '../TableListCell';
@@ -17,29 +17,19 @@ class TableUserRow extends React.Component<IUserRowProps> {
   state = { stateDialog: false, loadMore: false };
 
   // Set state for load more
-  handleLoadMore = () => {
+  handleToggleLoadMore = (): void => {
     this.setState({ loadMore: !this.state.loadMore });
   };
 
-  // Show RowMore component
-  isShowMore = (): boolean => {
-    const { projects } = this.props.user;
-
-    if (!projects || projects.length <= 2) {
-      return false;
-    }
-
-    return true;
-  };
-
   //Show dialog
-  handleShowDialog = () => {
+  handleToggleDialog = (): void => {
     this.setState({ stateDialog: !this.state.stateDialog });
   };
 
   render(): React.ReactNode {
     const { name, email, avatar, projects } = this.props.user;
     const { order } = this.props;
+    const shouldShowLoadMore = !(!projects || projects.length <= 2);
 
     return (
       <>
@@ -49,16 +39,17 @@ class TableUserRow extends React.Component<IUserRowProps> {
             <Avatar username={name} styles="circle" size="small" url={avatar} alt="avatar" />
           </td>
           <td>{email}</td>
-          <TableListCell listProject={projects} hasLoadMore={this.state.loadMore} />
+          {/* Render data of Role, Project, Status */}
+          <TableListCell listProject={projects as IProject[]} hasLoadMore={this.state.loadMore} />
 
           <td>
-            <a className="action" onClick={this.handleShowDialog}>
+            <a className="action" onClick={this.handleToggleDialog}>
               <img src={moreVert} />
             </a>
           </td>
         </tr>
-        {this.isShowMore() && (
-          <RowMore onClick={this.handleLoadMore} hasLoadMore={this.state.loadMore} />
+        {shouldShowLoadMore && (
+          <RowMore onClick={this.handleToggleLoadMore} hasLoadMore={this.state.loadMore} />
         )}
       </>
     );
