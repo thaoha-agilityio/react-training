3ae3f22a4c915fd80d3c4b3c256cdbb0bb001c07
Thao Y Ha <thao.ha@asnet.com.vn>
2022-11-08 09:34:31 +0700

    update function

--- practice-one/src/components/ConfirmModal/index.tsx
+++ practice-one/src/components/ConfirmModal/index.tsx
@@ -16,31 +16,33 @@ interface IProps {
   mainMessage: string;
 }
 
-class ConfirmModal extends React.Component<IProps> {
-  render() {
-    const { handleClose, id, users, handleConfirm, mainMessage } = this.props;
-
-    return (
-      <div className="overlay">
-        <div className="modal">
-          <div className="closeModal">
-            <a onClick={handleClose}>
-              <img src={closeIcon} alt="icon" />
-            </a>
-          </div>
-          <p>{mainMessage}</p>
-          <div className="button-wrapper">
-            <Button variant="primary" size="small" onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button variant="secondary" size="small" onClick={() => handleConfirm(users, id)}>
-              Save
-            </Button>
-          </div>
+const ConfirmModal = ({
+  handleClose,
+  id,
+  users,
+  handleConfirm,
+  mainMessage,
+}: IProps): React.ReactElement => {
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="closeModal">
+          <a onClick={handleClose}>
+            <img src={closeIcon} alt="icon" />
+          </a>
+        </div>
+        <p>{mainMessage}</p>
+        <div className="button-wrapper">
+          <Button variant="primary" size="small" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="secondary" size="small" onClick={() => handleConfirm(users, id)}>
+            Save
+          </Button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ConfirmModal;
