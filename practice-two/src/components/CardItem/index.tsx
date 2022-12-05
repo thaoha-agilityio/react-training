034544c65714fddeacc3cdbdef9cdf6fde57799c
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-05 10:30:25 +0700

    Implement cardItem component

---
+++ practice-two/src/components/CardItem/index.tsx
@@ -0,0 +1,25 @@
+import { BookRequire } from '@/types/book';
+import Avatar from '../Avatar';
+
+import './index.css';
+
+interface IProps {
+  book: BookRequire;
+}
+
+const CartItem = ({ book }: IProps) => {
+  return (
+    <div className="card-item">
+      <div className="card-image">
+        <Avatar url="" alt="" />
+      </div>
+      <div className="card-contents">
+        <h2 className="title"></h2>
+        <p className="info"></p>
+        <p className="info"></p>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
