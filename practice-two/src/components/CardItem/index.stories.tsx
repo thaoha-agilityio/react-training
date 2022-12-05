ca73eb87c64727233426be865c43fef7fe99b665
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-05 14:39:46 +0700

    Update storybook for CardItem component

--- practice-two/src/components/CardItem/index.stories.tsx
+++ practice-two/src/components/CardItem/index.stories.tsx
@@ -2,20 +2,29 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import CardItem from '.';
 
+const data = {
+  id: '1',
+  name: 'Taking Things Quietly',
+  avatar: 'http://digital.library.lse.ac.uk/objects/lse:dun949mif/view',
+  author: 'Francis ',
+  published: '1990',
+};
+
 export default {
   title: 'CardItem',
   component: CardItem,
+  argTypes: {
+    book: {
+      defaultValue: data,
+    },
+  },
 } as ComponentMeta<typeof CardItem>;
 
 const Template: ComponentStory<typeof CardItem> = (args) => <CardItem {...args} />;
 
 const Default = Template.bind({});
 Default.args = {
-  id: '1',
-  name: 'Taking Things Quietly',
-  avatar: 'http://digital.library.lse.ac.uk/objects/lse:dun949mif/view',
-  author: 'Francis ',
-  published: '1990',
+  book: data,
 };
 
 export { Default };
