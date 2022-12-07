5ed7dac3389e044955b0c0503f1b49b81ee72b56
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-07 09:20:56 +0700

    Update storybook for sidebar and category

--- practice-two/src/components/Category/index.stories.tsx
+++ practice-two/src/components/Category/index.stories.tsx
@@ -1,14 +1,11 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { CATEGORIES_BACKGROUND } from '@/constants/categories';
-
 import Category from '.';
 
 const data = {
   categoryName: 'Adventure',
   acronym: 'ad',
   total: 123,
-  background: CATEGORIES_BACKGROUND.LIGHTBLUE,
 };
 
 export default {
--- practice-two/src/components/SideBar/index.stories.tsx
+++ practice-two/src/components/SideBar/index.stories.tsx
@@ -1,7 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { CATEGORIES_BACKGROUND } from '@/constants/categories';
-
 import SideBar from '.';
 
 const data = [
@@ -9,13 +7,11 @@ const data = [
     categoryName: 'Adventure',
     acronym: 'ad',
     total: 123,
-    background: CATEGORIES_BACKGROUND.LIGHTBLUE,
   },
   {
     categoryName: 'Romance',
     acronym: 'rm',
     total: 123,
-    background: CATEGORIES_BACKGROUND.PINK,
   },
 ];
 
