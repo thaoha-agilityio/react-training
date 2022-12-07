ac02b21dafb906456e186a7d79e4b4cb9d6ee4ee
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-07 17:40:31 +0700

    Implement DetailModal component

---
+++ practice-two/src/components/Modal/DetailModal/index.tsx
@@ -0,0 +1,59 @@
+import Avatar from '@/components/Avatar';
+import Button from '@/components/Button';
+import Chip from '@/components/Chip';
+import SvgLightComponent from '@/components/Icon/SvgLightComponent';
+import SvgXmarkComponent from '@/components/Icon/SvgXmarkComponent';
+
+import { IBook } from '@/types/book';
+
+import './index.css';
+
+interface IPops {
+  book: IBook;
+}
+
+const DetailModal = ({
+  book: { name, avatar, author, description, publisher, published },
+}: IPops) => {
+  return (
+    <div className="modal-backdrop">
+      <div className="detail-modal">
+        <div className="modal-header">
+          <p className="modal-title">{name}</p>
+          <Button variant="outlined" icon={<SvgXmarkComponent />} />
+        </div>
+
+        <div className="modal-body">
+          <div className="modal-image">
+            <Avatar url={avatar} alt={name} size="medium" />
+          </div>
+          <p className="description">{description}</p>
+          <div className="details">
+            <p>Author :</p>
+            <Chip label={author} adornments={'endAdornments'} />
+          </div>
+          <div className="details">
+            <p>Published : </p>
+            <Chip label={published} adornments={'endAdornments'} />
+          </div>
+          <div className="details">
+            <p>Publishers:</p>
+            <Chip label={publisher || ''} adornments={'endAdornments'} />
+          </div>
+        </div>
+
+        <div className="modal-footer">
+          <div className="modal-wrapper">
+            <Button variant="secondary" text="esc" size="medium" />
+            <p>To Escape</p>
+          </div>
+          <div className="btn-background">
+            <Button variant="outlined" icon={<SvgLightComponent />} size="large" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailModal;
