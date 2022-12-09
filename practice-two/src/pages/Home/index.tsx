89c4569462ed715cb0b1558cbe062e70b5556488
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-09 09:45:47 +0700

    Show all book in view

--- practice-two/src/pages/Home/index.tsx
+++ practice-two/src/pages/Home/index.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react';
+
 import Header from '@/components/Header';
 import SubHeader from './SubHeader';
+import Books from './Books.tsx';
+
+import { getData } from '@/services/APIRequest';
+import { IBook } from '@/types/book';
+import { API_BASE_URL, API_PATH } from '@/constants/api';
 
 import './index.css';
 
 const Home = () => {
+  const [books, setBooks] = useState<IBook[]>([]);
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  const getBooks = async (): Promise<void> => {
+    const books: IBook[] = await getData(`${API_BASE_URL}${API_PATH}`);
+    setBooks(books);
+  };
+
   return (
     <div className="home">
       <div className="container">
         <Header />
         <SubHeader />
+        <div className="contents">
+          <Books books={books} />
+        </div>
       </div>
     </div>
   );
