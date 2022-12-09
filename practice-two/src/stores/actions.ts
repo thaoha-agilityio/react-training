cbbdf7aff8b95cca701018f63a6bc0a2dbbdcf35
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-09 17:30:31 +0700

    Add actions for books

---
+++ practice-two/src/stores/actions.ts
@@ -0,0 +1,15 @@
+import { ACTIONS } from '@/constants/actions';
+import { IBook } from '@/types/book';
+
+export type GetBooks = {
+  type: ACTIONS.GET_BOOKS;
+  payload: {
+    items: IBook[];
+  };
+};
+
+export type ChangeDarkMode = {
+  type: ACTIONS.CHANGE_DARK_MODE;
+};
+
+export type BooksAction = GetBooks | ChangeDarkMode;
