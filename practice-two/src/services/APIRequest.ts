2847ac621484325b1b3b2648678377f536e86cae
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-09 09:44:45 +0700

    Remove test function

--- practice-two/src/services/APIRequest.ts
+++ practice-two/src/services/APIRequest.ts
@@ -2,14 +2,4 @@ import axios from './config';
 
 const getData = <T>(url: string, params?: object) => axios.get<never, T>(url, params);
 
-const test = async (url: string): Promise<any> => {
-  await axios({
-    method: 'get',
-    url,
-    responseType: 'stream',
-  }).then(function (response) {
-    response.data;
-  });
-};
-
-export { getData, test };
+export { getData };
