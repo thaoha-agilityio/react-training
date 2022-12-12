1665bf7a96d92e5df7151f5a09d2bd17dd50c28a
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-12 15:46:06 +0700

    Update props books into idBooks

--- practice-two/src/pages/Home/Books.tsx/index.tsx
+++ practice-two/src/pages/Home/Books.tsx/index.tsx
@@ -1,20 +1,16 @@
 import CardItem from '@/components/CardItem';
 
-import { IBook } from '@/types/book';
-
 import './index.css';
 
 interface IProps {
-  books: IBook[];
+  idBooks: string[];
 }
 
-const Books = ({ books }: IProps) => {
+const Books = ({ idBooks }: IProps) => {
   return (
     <div className="books">
-      {books.map((book) => (
-        <div key={book.id}>
-          <CardItem book={book} />
-        </div>
+      {idBooks.map((id: any) => (
+        <div key={id}>{<CardItem idBook={id} />}</div>
       ))}
     </div>
   );
