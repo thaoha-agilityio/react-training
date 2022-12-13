1758c30df70b3401c3e9085285d408545306b5cf
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-13 11:31:36 +0700

    Add getBookById action

---
+++ practice-two/src/stores/books/reducers.ts
@@ -0,0 +1,37 @@
+import { IBook } from '@/types/book';
+import { ACTIONS } from '@/constants/actions';
+import { BooksAction } from './actions';
+
+export interface BooksState {
+  books: IBook[];
+  book: IBook | null;
+  ids: string[];
+}
+
+const initialState: BooksState = {
+  books: [],
+  book: null,
+  ids: [],
+};
+
+const booksReducer = (state: BooksState = initialState, actions: BooksAction): BooksState => {
+  switch (actions.type) {
+    case ACTIONS.GET_BOOKS:
+      return {
+        ...state,
+        books: actions.payload.books,
+        ids: actions.payload.ids,
+      };
+
+    case ACTIONS.GET_BOOK_BY_ID:
+      return {
+        ...state,
+        book: actions.payload.book,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export { initialState, booksReducer };
