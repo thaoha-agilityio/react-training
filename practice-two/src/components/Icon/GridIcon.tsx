fa5dad7c005f614d799ece581eac7186075dd467
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-15 11:46:38 +0700

    Add Icon components

---
+++ practice-two/src/components/Icon/GridIcon.tsx
@@ -0,0 +1,10 @@
+import { SVGProps } from 'react';
+
+export const GridIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={12} height={12} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M4 0H1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1Zm0 4H1V1h3v3ZM11 0H8a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1Zm0 4H8V1h3v3ZM4 7H1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1Zm0 4H1V8h3v3ZM11 7H8a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1Zm0 4H8V8h3v3Z"
+      fill="#A6B0C3"
+    />
+  </svg>
+);
