c08222cd51752272fe4ec9f070d0b346ddfecf48
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-19 16:08:39 +0700

    Generate url of filter

--- practice-two/src/helper/filter.ts
+++ practice-two/src/helper/filter.ts
@@ -2,10 +2,11 @@ import { API_PATH, API_BASE_URL } from '@/constants/api';
 
 type FilterType = {
   searchInput?: string;
+  categoriesId?: string[];
 };
 
 export const generateUrl = (filterOption: FilterType) => {
-  const { searchInput } = filterOption;
+  const { searchInput, categoriesId } = filterOption;
 
   let result = `${API_BASE_URL}${API_PATH.books}`;
 
@@ -13,6 +14,8 @@ export const generateUrl = (filterOption: FilterType) => {
     case searchInput && searchInput !== '':
       return (result += `?name=${searchInput}`);
 
+    case !!categoriesId:
+      return (result += `?category=[${categoriesId}]`);
     default:
       return result;
   }
