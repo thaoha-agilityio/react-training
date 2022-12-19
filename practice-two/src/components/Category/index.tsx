e58a3a878422f74a08318c8c85c8205442b65e38
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-19 15:58:46 +0700

    Handle get id and show active

--- practice-two/src/components/Category/index.tsx
+++ practice-two/src/components/Category/index.tsx
@@ -1,24 +1,35 @@
+import { CategoriesContext } from '@/contexts/CategoriesContext';
 import { generateColor } from '@/helper/randomColor';
 import { ICategory } from '@/types/category';
+import { memo, useContext } from 'react';
 
 import './index.css';
 
 interface IProps {
   category: ICategory;
+  onSelectId: (id: string) => void;
 }
 
-const Category = ({ category: { name, total } }: IProps): React.ReactElement => {
+const Category = ({ category: { id, name, total }, onSelectId }: IProps): React.ReactElement => {
+  const { categoriesId } = useContext(CategoriesContext);
+
+  const handleSelectId = () => {
+    onSelectId(id);
+  };
+
   return (
-    <div className="category-wrapper">
-      <div className="category">
-        <div className="thumbnail" style={{ backgroundColor: generateColor() }}>
-          <p>{name.substring(0, 2)}</p>
+    <div className={`${categoriesId.includes(id) ? 'active' : ''}`}>
+      <div className="category-wrapper" onClick={handleSelectId}>
+        <div className="category">
+          <div className="thumbnail" style={{ backgroundColor: generateColor() }}>
+            <p>{name.substring(0, 2)}</p>
+          </div>
+          <p className="category-name">{name}</p>
         </div>
-        <p className="category-name">{name}</p>
+        <p className="total">{total}</p>
       </div>
-      <p className="total">{total.toString()}</p>
     </div>
   );
 };
 
-export default Category;
+export default memo(Category);
