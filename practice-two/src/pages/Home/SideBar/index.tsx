d77ba16efc6369c09f086a9aa7cde593707075b1
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-19 15:59:37 +0700

    Filter books when click button

--- practice-two/src/pages/Home/SideBar/index.tsx
+++ practice-two/src/pages/Home/SideBar/index.tsx
@@ -1,4 +1,7 @@
-import { memo } from 'react';
+import { memo, useContext } from 'react';
+
+import { CategoriesContext } from '@/contexts/CategoriesContext';
+import { BooksContext } from '@/contexts/BooksContext';
 
 import Category from '../../../components/Category';
 
@@ -11,12 +14,23 @@ interface IProps {
 }
 
 const SideBar = ({ categories }: IProps): React.ReactElement => {
+  const { getSelectedId, categoriesId } = useContext(CategoriesContext);
+  const { filterByCategories } = useContext(BooksContext);
+
+  // Get categoryId when click
+  const handleSelectedId = (id: string) => {
+    getSelectedId(id);
+
+    // Show books after filter
+    filterByCategories([...categoriesId, id]);
+  };
+
   return (
     <div className="categories">
       <p className="paraphrase">A curated list of every book ever written</p>
       {categories?.map((category: ICategory) => (
         <div key={category.id}>
-          <Category key={category.name} category={category} />
+          <Category key={category.name} category={category} onSelectId={handleSelectedId} />
         </div>
       ))}
     </div>
