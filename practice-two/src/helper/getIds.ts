59d03bccc35bc651193c44994f66e5e30b3ab0bd
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-28 14:30:25 +0700

    Remove getListMatch function

--- practice-two/src/helper/getIds.ts
+++ practice-two/src/helper/getIds.ts
@@ -1,8 +1,3 @@
 // Get ids from list
 export const getIdsFromList = <T extends { id: string }>(array: T[]): string[] =>
   array.map((item) => item.id);
-
-// Find ids matches between two arrays
-export const getListMatch = <T extends { id: string }>(firstArray: T[], secondArray: string[]) => {
-  return firstArray.filter((item) => secondArray.indexOf(item.id) !== -1);
-};
