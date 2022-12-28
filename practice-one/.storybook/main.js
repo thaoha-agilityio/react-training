26c033cf9f573ebba928cfc1b735566ade34560e
Thao Y Ha <thao.ha@asnet.com.vn>
2022-12-28 10:09:41 +0700

    config storybook

--- practice-one/.storybook/main.js
+++ practice-one/.storybook/main.js
@@ -1,13 +1,49 @@
+const path = require('path');
+const svgrPlugin = require('vite-plugin-svgr');
+const { loadConfigFromFile, mergeConfig } = require('vite');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
+    'storybook-addon-react-router-v6',
+    '@storybook/addon-contexts/register',
   ],
   framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-webpack5',
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtracLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+  async viteFinal(config, { configType }) {
+    const customPlugins = [
+      svgrPlugin({
+        svgrOptions: {
+          icon: true,
+        },
+      }),
+    ];
+
+    const { config: userConfig } = await loadConfigFromFile(
+      path.resolve(__dirname, '../vite.config.ts')
+    );
+
+    return mergeConfig(config, {
+      ...userConfig,
+
+      plugins: [...customPlugins],
+    });
   },
 };
