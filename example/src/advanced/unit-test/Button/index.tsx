89e7be28a865b3e9abfb1829c511c024476fd074
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-03 17:17:45 +0700

    Add example unit test

---
+++ example/src/advanced/unit-test/Button/index.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+
+import "./index.css";
+
+interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: "primary" | "secondary" | "tertiary";
+  size: "small" | "medium" | "large" | "radius";
+  styles: "normal" | "circle";
+  text?: string;
+  icon?: ReactNode;
+}
+
+const Button = ({
+  variant,
+  size,
+  icon,
+  text,
+  styles,
+  ...rest
+}: IProps): React.ReactElement => (
+  <button
+    className={`$ btn btn-${variant} btn-${size} btn-${styles}`}
+    {...rest}
+  >
+    {icon}
+    {text}
+  </button>
+);
+
+export default Button;
