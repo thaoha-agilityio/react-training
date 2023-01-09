dfc5eef23781cb83bda99926d05db1fba722bda5
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-09 11:50:00 +0700

    Init workspace

---
+++ practice-three/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import EnvironmentPlugin from "vite-plugin-environment";
+import path from "path";
+import svgr from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [react(), svgr(), EnvironmentPlugin("all")],
+});
