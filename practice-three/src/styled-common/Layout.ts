e34514113563586e7ac92f20884f674f7a9c1940
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-11 13:42:50 +0700

    Add styled common

---
+++ practice-three/src/styled-common/Layout.ts
@@ -0,0 +1,23 @@
+import styled, { css } from "styled-components";
+
+const Container = styled.div`
+  max-width: 1273px;
+  margin: auto;
+`;
+
+const AlignCenterStyle = css`
+  display: flex;
+  align-items: center;
+`;
+
+const JustifyBetweenStyle = css`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ColumnCenterStyle = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+export { Container, AlignCenterStyle, JustifyBetweenStyle, ColumnCenterStyle };
