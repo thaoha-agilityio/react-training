19658e68033ec294624234de6c218f6b9c322bd8
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-11 17:37:14 +0700

    Import theme provider

--- practice-three/src/main.tsx
+++ practice-three/src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+
+import { themes } from "@/themes";
 import App from "./App";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themes}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
