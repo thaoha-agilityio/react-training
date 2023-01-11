53c56f281700f1f77e29d31bed907e7495770cb8
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-11 16:55:09 +0700

    Add spacing

--- practice-three/src/themes/metrics.ts
+++ practice-three/src/themes/metrics.ts
@@ -1,17 +1,46 @@
 const widths = {
-  tiny: "40px",
-  small: "116px",
-  medium: "268px",
-  large: "485px",
-  extraLarge: "908px",
+  xxs: "40px",
+  xs: "123px",
+  sm: "201px",
+  md: "236px",
+  lg: "282px",
+  xl: "353px",
+  xxl: "485px",
+  "3xl": "908px",
 };
 
 const heights = {
-  tiny: "40px",
-  small: "213px",
-  medium: "369px",
-  large: "485px",
-  extraLarge: "670px",
+  xxs: "40px",
+  xs: "45px",
+  sm: "56px",
+  md: "114px",
+  lg: "202px",
+  xl: "213px",
+  xxl: "238px",
+  "3xl": "331px",
+  "4xl": "369px",
 };
 
-export { widths, heights };
+const margins = {
+  xxs: "5x",
+  xs: "6px",
+  sm: "10px",
+  md: "12px",
+  lg: "15px",
+  xl: "20px",
+  xxl: "25px",
+};
+
+const paddings = {
+  xxs: "5x",
+  xs: "8px",
+  sm: "10px",
+  md: "12px",
+  lg: "15px",
+  xl: "20px",
+  xxl: "25px",
+  "3xl": "40px",
+  "4xl": "45px",
+};
+
+export { widths, heights, margins, paddings };
