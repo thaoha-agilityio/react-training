8b25ddb904ead7787d50f1d0408a64466f888d87
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-11 11:53:15 +0700

    Create theme styled

---
+++ practice-three/src/Theme/font.ts
@@ -0,0 +1,33 @@
+const fontSizes = {
+  xxs: "11px",
+  xs: "12px",
+  sm: "13px",
+  md: "14px",
+  lg: "18px",
+  xl: "24px",
+  xxl: "36px",
+};
+
+const lineHeights = {
+  none: "1",
+  shorter: "1.15",
+  short: "1.2",
+  normal: "1.38",
+  base: "1.55",
+  tall: "1.7",
+  taller: "2",
+};
+
+const fontFamilies = {
+  primary: "Rubik",
+  secondary: "DM_Sans",
+  tertiary: "Noto_Sans",
+};
+
+const fontWeights = {
+  normal: 400,
+  semiBold: 500,
+  bold: 600,
+};
+
+export { fontSizes, lineHeights, fontFamilies, fontWeights };
---
+++ practice-three/src/Theme/metrics.ts
@@ -0,0 +1,17 @@
+const widths = {
+  tiny: "40px",
+  small: "116px",
+  medium: "268px",
+  large: "485px",
+  extraLarge: "908px",
+};
+
+const heights = {
+  tiny: "40px",
+  small: "213px",
+  medium: "369px",
+  large: "485px",
+  extraLarge: "670px",
+};
+
+export { widths, heights };
