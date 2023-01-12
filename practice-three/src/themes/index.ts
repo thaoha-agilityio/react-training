6089a13af7a9ac003b05f9353244dcbaaf8f3f0f
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-12 15:56:38 +0700

    Export themes all

--- practice-three/src/themes/index.ts
+++ practice-three/src/themes/index.ts
@@ -1,15 +1,3 @@
-import { colors } from "./colors";
-import { fontSizes, fontWeights, fontFamilies, lineHeights } from "./font";
-import { widths, heights, margins, paddings } from "./metrics";
-
-export const themes = {
-  colors,
-  fontSizes,
-  fontWeights,
-  fontFamilies,
-  lineHeights,
-  widths,
-  heights,
-  margins,
-  paddings,
-};
+export * from "./colors";
+export * as fonts from "./fonts";
+export * as metrics from "./metrics";
