ac2f8fd960f39a0decef2eb08f80664621e4a614
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-13 17:28:58 +0700

    Add styled for input component

---
+++ practice-three/src/components/Input/index.styled.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+import { metrics, fonts } from "../../themes";
+
+interface StyledInputProps {
+  width: number;
+  height: number;
+  borderRadius: number;
+  pLeft: number;
+}
+
+const InputGroupStyled = styled.div`
+  display: flex;
+  position: relative;
+`;
+
+const InputLeftElementStyled = styled.div`
+  position: absolute;
+  z-index: 1;
+  padding-top: ${metrics.paddings.md}px;
+  padding-left: ${metrics.paddings.lg}px;
+`;
+
+const InputStyled = styled.input<StyledInputProps>`
+  ${(props) => props.width && `width:${props.width}px`};
+  ${(props) => props.height && `height:${props.height}px`};
+  ${(props) => props.borderRadius && `border-radius:${props.borderRadius}px`};
+  ${(props) => props.pLeft && `padding-left:${props.pLeft}px`};
+  font-family: ${fonts.fontFamilies.primary};
+  box-shadow: ${metrics.shadows.sm};
+  font-size: ${fonts.fontSizes.sm}px;
+  line-height: ${fonts.lineHeights.tiny};
+  border: none;
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.24);
+  }
+`;
+
+export { InputGroupStyled, InputLeftElementStyled, InputStyled };
