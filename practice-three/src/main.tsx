f6b6ceaaf28c3c46a8f886c62758867bb6ad8155
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-13 10:01:19 +0700

    Remove ThemeProvide

--- practice-three/src/main.tsx
+++ practice-three/src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 
-import { themes } from "@/themes";
 import App from "./App";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={themes}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
