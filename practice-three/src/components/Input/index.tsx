74520259f61631f904b2cb7fdb5a9dca29657144
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-13 17:29:31 +0700

    Implement Input component

---
+++ practice-three/src/components/Input/index.tsx
@@ -0,0 +1,45 @@
+import { ComponentPropsWithRef, ReactNode } from "react";
+
+import {
+  InputGroupStyled,
+  InputLeftElementStyled,
+  InputStyled,
+} from "./index.styled";
+
+interface IProps extends Omit<ComponentPropsWithRef<"input">, "size"> {
+  width: number;
+  height: number;
+  borderRadius: number;
+  pLeft: number;
+  children?: ReactNode;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({
+  width,
+  height,
+  borderRadius,
+  type,
+  pLeft,
+  children,
+  onChange,
+  ...rest
+}: IProps) => {
+  return (
+    <InputGroupStyled>
+      <InputLeftElementStyled children={children} />
+      <InputStyled
+        type={type}
+        width={width}
+        height={height}
+        onChange={onChange}
+        borderRadius={borderRadius}
+        placeholder="Search book"
+        pLeft={pLeft}
+        {...rest}
+      />
+    </InputGroupStyled>
+  );
+};
+
+export default Input;
