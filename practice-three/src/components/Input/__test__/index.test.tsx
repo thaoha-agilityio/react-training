4fa74e24c17bad88c196e9e081e127c6f158160b
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-16 09:48:53 +0700

    Add unit test for input component

--- practice-three/src/components/Input/__test__/index.test.tsx
+++ practice-three/src/components/Input/__test__/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import { SearchIcon } from "../../Icon";
