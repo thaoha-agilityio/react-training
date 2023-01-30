aacc5185164dce77c37eb6b17da74088c37425ee
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-30 09:58:33 +0700

    Update storybook

--- practice-three/src/components/Category/index.stories.tsx
+++ practice-three/src/components/Category/index.stories.tsx
@@ -1,3 +1,4 @@
+import { category } from "@/constants/mockData";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Category from ".";
@@ -5,8 +6,15 @@ import Category from ".";
 export default {
   title: "Category",
   component: Category,
+  argTypes: {
+    category: {
+      defaultValue: category,
+    },
+  },
 } as ComponentMeta<typeof Category>;
 
-const Template: ComponentStory<typeof Category> = (args) => <Category />;
+const Template: ComponentStory<typeof Category> = (args) => (
+  <Category {...args} />
+);
 
 export const Default = Template.bind({});
