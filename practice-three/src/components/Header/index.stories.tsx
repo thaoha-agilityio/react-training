baec67eb4aba017a832fa0733bec03032d3ba3ea
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-31 16:42:18 +0700

    Add storybook for header component

---
+++ practice-three/src/components/Header/index.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import Header from ".";
+
+export default {
+  title: "Header",
+  component: Header,
+  argTypes: {},
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = () => <Header />;
+
+export const Default = Template.bind({});
