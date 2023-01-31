d442c586039f214e323386b6c4ef884bee58c5bd
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-31 11:14:05 +0700

    Apply memo

--- practice-three/src/components/Modal/FilterModal/index.tsx
+++ practice-three/src/components/Modal/FilterModal/index.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 import Button from "../../Button";
 import Chip from "../../Chip";
 import { GridIcon, ListIcon, ArrowIcon } from "../../Icon";
@@ -26,57 +28,55 @@ const FilterModal = ({
   height,
   top,
   right,
-}: IProps): React.ReactElement => {
-  return (
-    <BackDropStyled>
-      <FilterModalStyled width={width} height={height} right={right} top={top}>
-        <MenuOptionStyled>Display Options</MenuOptionStyled>
-        <MenuListStyled>
-          <MenuOptionGroup>
-            <Button
-              isCircle
-              icon={<ListIcon />}
-              bgColor="transparent"
-              borderColor={colors.gray[20]}
-            />
-            <OptionalTextStyled>list</OptionalTextStyled>
-          </MenuOptionGroup>
-          <MenuOptionGroup>
-            <Button
-              isCircle
-              icon={<GridIcon />}
-              bgColor="transparent"
-              borderColor={colors.gray[20]}
-            />
-            <OptionalTextStyled>grid</OptionalTextStyled>
-          </MenuOptionGroup>
-        </MenuListStyled>
-        <SortOptionStyled>
-          <MenuOptionStyled>sort by</MenuOptionStyled>
-          <Chip
-            label="alphabetical order"
-            color={colors.dark[200]}
-            width={225}
-            height={46}
-            fontSize={13}
-            fontWeight={400}
-            flexLayout
-            endAdornments={<ArrowIcon />}
+}: IProps): React.ReactElement => (
+  <BackDropStyled>
+    <FilterModalStyled width={width} height={height} right={right} top={top}>
+      <MenuOptionStyled>Display Options</MenuOptionStyled>
+      <MenuListStyled>
+        <MenuOptionGroup>
+          <Button
+            isCircle
+            icon={<ListIcon />}
+            bgColor="transparent"
+            borderColor={colors.gray[20]}
           />
-          <Chip
-            label="release year"
-            color={colors.dark[200]}
-            width={225}
-            height={46}
-            fontSize={13}
-            fontWeight={400}
-            flexLayout
-            endAdornments={<ArrowIcon />}
+          <OptionalTextStyled>list</OptionalTextStyled>
+        </MenuOptionGroup>
+        <MenuOptionGroup>
+          <Button
+            isCircle
+            icon={<GridIcon />}
+            bgColor="transparent"
+            borderColor={colors.gray[20]}
           />
-        </SortOptionStyled>
-      </FilterModalStyled>
-    </BackDropStyled>
-  );
-};
+          <OptionalTextStyled>grid</OptionalTextStyled>
+        </MenuOptionGroup>
+      </MenuListStyled>
+      <SortOptionStyled>
+        <MenuOptionStyled>sort by</MenuOptionStyled>
+        <Chip
+          label="alphabetical order"
+          color={colors.dark[200]}
+          width={225}
+          height={46}
+          fontSize={13}
+          fontWeight={400}
+          flexLayout
+          endAdornments={<ArrowIcon />}
+        />
+        <Chip
+          label="release year"
+          color={colors.dark[200]}
+          width={225}
+          height={46}
+          fontSize={13}
+          fontWeight={400}
+          flexLayout
+          endAdornments={<ArrowIcon />}
+        />
+      </SortOptionStyled>
+    </FilterModalStyled>
+  </BackDropStyled>
+);
 
-export default FilterModal;
+export default memo(FilterModal);
