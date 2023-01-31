ee4ab2af514a361a5c8bda4c7a8ae606faf62d41
Thao Y Ha <thao.ha@asnet.com.vn>
2023-01-31 15:00:26 +0700

    Implement DetailModal component

---
+++ practice-three/src/components/Modal/DetailModal/index.styled.ts
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+
+import { AlignBaseLineStyle, JustifyBetweenStyle, P } from "@/styled-common";
+import { colors, fonts, metrics } from "@/themes";
+
+const DetailModalStyled = styled.div`
+  position: absolute;
+  right: 0;
+  width: ${metrics.widths.xxl}px;
+  min-height: calc(100vh - 0px);
+  box-shadow: ${metrics.shadows.lg};
+  border-radius: ${metrics.borderRadius.tiny};
+`;
+
+const ModalHeaderStyled = styled.div`
+  ${JustifyBetweenStyle};
+  width: ${metrics.widths.xxl};
+  height: 68px;
+  padding: 20px 29px;
+  background-color: ${colors.gray[30]};
+`;
+
+const ModalTitleStyled = styled(P.Large)`
+  color: ${colors.dark[100]};
+`;
+
+const ModalBodyStyled = styled.div`
+  padding: 39px 41px 31px;
+  min-height: calc(100vh - 122px);
+  background-color: white;
+`;
+
+const ModalImageStyled = styled.div`
+  text-align: center;
+  margin-bottom: ${metrics.margins.xxl};
+`;
+
+const DescriptionTextStyled = styled(P.Tiny)`
+  color: ${colors.gray[150]};
+  line-height: ${fonts.lineHeights.lg};
+  margin-bottom: 9px;
+  width: 413px;
+`;
+
+const BookInfoStyled = styled.div`
+  ${AlignBaseLineStyle};
+  gap: 10px;
+  margin-top: 15px;
+`;
+
+const MainTextStyled = styled(P.Normal)`
+  color: ${colors.dark[50]};
+`;
+
+const BackgroundTextStyled = styled.div`
+  background: ${colors.blue[20]};
+  border-radius: ${metrics.borderRadius.lg}px;
+  padding: 5px 15px;
+`;
+
+const TextInfoStyled = styled(P.Normal)`
+  color: ${colors.blue[250]};
+`;
+
+const ModalFooterStyled = styled.div`
+  display: flex;
+  justify-content: end;
+  gap: 39px;
+  height: 58px;
+  padding: 12px 19px;
+  background-color: ${colors.gray[30]};
+`;
+
+const WrapperButtonStyled = styled.div`
+  ${AlignBaseLineStyle};
+  gap: 9px;
+`;
+
+const EscapeTextStyled = styled.p`
+  font-family: ${fonts.fontFamilies.secondary};
+  font-weight: ${fonts.fontWeights.medium};
+  font-size: ${fonts.fontSizes.xs}px;
+  color: ${colors.gray[150]};
+  line-height: 1.3;
+  text-transform: capitalize;
+`;
+
+export {
+  DetailModalStyled,
+  ModalHeaderStyled,
+  ModalTitleStyled,
+  ModalBodyStyled,
+  ModalImageStyled,
+  DescriptionTextStyled,
+  BookInfoStyled,
+  MainTextStyled,
+  BackgroundTextStyled,
+  TextInfoStyled,
+  ModalFooterStyled,
+  WrapperButtonStyled,
+  EscapeTextStyled,
+};
---
+++ practice-three/src/components/Modal/DetailModal/index.tsx
@@ -0,0 +1,93 @@
+import { memo } from "react";
+
+import Avatar from "@/components/Avatar";
+import Button from "@/components/Button";
+import ToggleButton from "@/components/ToggleButton";
+import LabelButton from "@/pages/Home/components/LabelButton";
+import { LightIcon, XmarkIcon } from "@/components/Icon";
+
+import { Backdrop } from "@/styled-common";
+import { IBook } from "@/types/book";
+
+import {
+  DetailModalStyled,
+  ModalHeaderStyled,
+  ModalTitleStyled,
+  ModalBodyStyled,
+  ModalImageStyled,
+  DescriptionTextStyled,
+  BookInfoStyled,
+  MainTextStyled,
+  BackgroundTextStyled,
+  TextInfoStyled,
+  ModalFooterStyled,
+  WrapperButtonStyled,
+  EscapeTextStyled,
+} from "./index.styled";
+
+interface IProps {
+  book: IBook;
+}
+
+const DetailModal = ({
+  book: { name, avatar, author, description, published, publishers },
+}: IProps): React.ReactElement => (
+  <Backdrop>
+    <DetailModalStyled>
+      <ModalHeaderStyled>
+        <ModalTitleStyled>{name}</ModalTitleStyled>
+        <Button isCircle={false} bgColor="transparent" icon={<XmarkIcon />} />
+      </ModalHeaderStyled>
+
+      <ModalBodyStyled>
+        <ModalImageStyled>
+          <Avatar
+            url={avatar}
+            alt={name}
+            borderRadius={5}
+            width={201}
+            height={263}
+          />
+        </ModalImageStyled>
+        <DescriptionTextStyled>{description}</DescriptionTextStyled>
+
+        <BookInfoStyled>
+          <MainTextStyled>Author :</MainTextStyled>
+          <BackgroundTextStyled>
+            <TextInfoStyled>{author}</TextInfoStyled>
+          </BackgroundTextStyled>
+        </BookInfoStyled>
+
+        <BookInfoStyled>
+          <MainTextStyled>Published :</MainTextStyled>
+          <BackgroundTextStyled>
+            <TextInfoStyled>{published}</TextInfoStyled>
+          </BackgroundTextStyled>
+        </BookInfoStyled>
+
+        <BookInfoStyled>
+          <MainTextStyled>Publishers :</MainTextStyled>
+          <BackgroundTextStyled>
+            <TextInfoStyled>{publishers}</TextInfoStyled>
+          </BackgroundTextStyled>
+        </BookInfoStyled>
+      </ModalBodyStyled>
+
+      <ModalFooterStyled>
+        <WrapperButtonStyled>
+          <LabelButton text="esc" />
+          <EscapeTextStyled>to escape</EscapeTextStyled>
+        </WrapperButtonStyled>
+        <ToggleButton
+          width={77}
+          height={34}
+          textAlign="left"
+          borderRadius={30}
+          icon={<LightIcon />}
+        />
+      </ModalFooterStyled>
+    </DetailModalStyled>
+  </Backdrop>
+);
+
+export default memo(DetailModal);
