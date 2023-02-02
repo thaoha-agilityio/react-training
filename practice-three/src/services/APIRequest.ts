20b8ffac3aff9b0ceee7579b89405c3e5c36dbfb
Thao Y Ha <thao.ha@asnet.com.vn>
2023-02-02 17:29:41 +0700

    Create get data

--- practice-three/src/services/APIRequest.ts
+++ practice-three/src/services/APIRequest.ts
@@ -1,6 +1,12 @@
-import axios from "./config";
+import axios from "axios";
 
-const getData = <T>(url: string, params?: object) =>
-  axios.get<never, T>(url, params);
+// const getData =async <T>(url: string, params?: object) =>
+//   axios.get<never, T>(url, params);
+
+const getData = async <T>(url: string): Promise<T> => {
+  const res = await axios.get<T>(url);
+  console.log("get Data: ", res.data);
+  return res.data;
+};
 
 export { getData };
