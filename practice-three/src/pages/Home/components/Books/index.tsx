46b611a021b125dacb3fcb7ebce43ea5998aeff4
Thao Y Ha <thao.ha@asnet.com.vn>
2023-02-03 09:20:31 +0700

    Show message when array empty

--- practice-three/src/pages/Home/components/Books/index.tsx
+++ practice-three/src/pages/Home/components/Books/index.tsx
@@ -7,17 +7,20 @@ import { BooksContext } from "@/contexts/BooksContext";
 
 // Styled
 import { BooksStyled } from "./index.styled";
+import { NOTICE_MESSAGE } from "@/constants/message";
+import { P } from "@/styled-common";
 
 const Books = (): React.ReactElement => {
   const { books } = useContext(BooksContext);
 
   return (
     <BooksStyled>
-      <>
-        {books.map((item) => (
-          <div key={item.id}>{<CardItem item={item} />}</div>
-        ))}
-      </>
+      {books.map((item) => (
+        <div key={item.id}>{<CardItem item={item} />}</div>
+      ))}
+
+      {/* Show message when array empty */}
+      {books.length === 0 && <P.Normal>{NOTICE_MESSAGE}</P.Normal>}
     </BooksStyled>
   );
 };
