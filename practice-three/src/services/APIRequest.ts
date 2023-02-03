e95f8971fd95b53cfb735c8d3dd3215f5b8b5d94
Thao Y Ha <thao.ha@asnet.com.vn>
2023-02-03 08:40:28 +0700

    Remove code comment

--- practice-three/src/services/APIRequest.ts
+++ practice-three/src/services/APIRequest.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getData = async <T>(url: string): Promise<T> => {
   const res = await axios.get<T>(url);
-  console.log("get Data: ", res.data);
+
   return res.data;
 };
 
