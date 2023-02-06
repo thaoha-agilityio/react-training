b575d96986c348d5cb35ec1e363f8398d5dcf2e6
Thao Y Ha <thao.ha@asnet.com.vn>
2023-02-06 10:01:00 +0700

    Add case the error case

--- practice-three/src/services/APIRequest.ts
+++ practice-three/src/services/APIRequest.ts
@@ -1,9 +1,20 @@
 import axios from "axios";
+import { ERROR_MESSAGE } from "../constants/message";
 
-const getData = async <T>(url: string): Promise<T> => {
-  const res = await axios.get<T>(url);
+const getData = async <T>(url: string): Promise<T | undefined> => {
+  try {
+    const res = await axios.get<T>(url);
 
-  return res.data;
+    return res.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+
+    alert(ERROR_MESSAGE);
+  }
 };
 
-export { getData };
+const api = { getData };
+
+export { api };
