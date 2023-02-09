9b2024a45196c46ee104a3c0fc818a0986339dd0
Thao Y Ha <thao.ha@asnet.com.vn>
2023-02-09 15:04:55 +0700

    Update generateUrl function

--- practice-three/src/utils/generateUrl.ts
+++ practice-three/src/utils/generateUrl.ts
@@ -1,26 +1,15 @@
-import { API_PATH, API_BASE_URL } from "@/constants/api";
+import { API_PATH, API_BASE_URL } from "../constants/api";
 
 type FilterType = {
   key: string;
-  queryString?: string;
-  queryStringArr?: string[];
+  value: string | string[];
 };
 
 export const generateUrl = (filterOption: FilterType): string => {
-  const { key, queryString, queryStringArr } = filterOption;
+  const { key, value } = filterOption;
+  const url = `${API_BASE_URL}${API_PATH.BOOKS}`;
 
-  let url = `${API_BASE_URL}${API_PATH.BOOKS}`;
+  if (typeof value === "string") return `${url}?${key}=${value}`;
 
-  // Add query params to GET request
-  switch (true) {
-    case !!queryString && queryString !== " ":
-      url += `?${key}=${queryString}`;
-      break;
-
-    case !!queryStringArr:
-      url += `?${key}=[${queryStringArr}]`;
-      break;
-  }
-
-  return url;
+  return `${url}?${key}=[${value}]`;
 };
