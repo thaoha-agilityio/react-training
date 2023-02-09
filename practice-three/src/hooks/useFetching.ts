ede4628f9102f942aab688722c3fc5aa569d7f46
Thao Y Ha <thao.ha@asnet.com.vn>
2023-02-09 15:04:27 +0700

    Add return type for useFetching

--- practice-three/src/hooks/useFetching.ts
+++ practice-three/src/hooks/useFetching.ts
@@ -3,12 +3,18 @@ import useSWRImmutable from "swr/immutable";
 // API call
 import { api } from "../services/APIRequest";
 
-export const useFetching = <T>(url: string) => {
+export const useFetching = <T>(
+  url: string
+): {
+  data: T;
+  error: string;
+} => {
   const fetcher = async () => (await api.getData<T>(url)).data;
+
   const { data, error } = useSWRImmutable<T>(url, fetcher);
 
   return {
-    data,
+    data: data as T,
     error,
   };
 };
