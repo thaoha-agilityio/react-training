cb51ac5043fec024bc8c0ca107ed069131285bf3
Thao Y Ha <thao.ha@asnet.com.vn>
2023-02-09 15:02:55 +0700

    Update import path

--- practice-three/src/contexts/BooksContext.tsx
+++ practice-three/src/contexts/BooksContext.tsx
@@ -6,17 +6,17 @@ import {
   useMemo,
   useState,
 } from "react";
-import { useFetching } from "@/hooks";
 
 import { API_BASE_URL, API_PATH } from "@/constants/api";
 import { generateUrl } from "@/utils/generateUrl";
 
 import { api } from "@/services/APIRequest";
 import { IBook } from "@/types/book";
+import { useFetching } from "@/hooks";
 
 type IBookContext = {
   books: IBook[];
-  error: "";
+  error: string;
   searchBooks: (input: string) => Promise<void>;
 };
 
@@ -32,45 +32,32 @@ export const BooksProvider = ({ children }: IBookProvider) => {
   const [books, setBooks] = useState<IBook[]>([]);
 
   // Fetch data from server
-  // const { data: items, error } = useFetching<IBook[]>(
-  //   `${API_BASE_URL}${API_PATH.BOOKS}`
-  // );
 
-  const getBooks = async () => {
-    const books = await api.getData(`${API_BASE_URL}${API_PATH.BOOKS}`);
-  };
-  useEffect(() => {}, []);
+  const { data: items, error } = useFetching<IBook[]>(
+    `${API_BASE_URL}${API_PATH.BOOKS}`
+  );
 
-  // console.log("items", items);
-  // console.log("error", error);
-  // useEffect(() => {
-  //   if (!items && !error) {
-  //     setBooks(items);
-  //   }
-  // }, [items, error]);
+  useEffect(() => {
+    setBooks(items);
+  }, [!items, !error]);
 
   // Search by call api
   const searchBooks = useCallback(async (input: string): Promise<void> => {
     const result = await api.getData(
-      generateUrl({ key: "name", queryString: input })
+      generateUrl({ key: "name", value: input })
     );
+
     if (result.error) return setBooks([]);
 
     setBooks(result.data);
   }, []);
 
-  // const contextValue: IBookContext = {
-  //   books: books || [],
-  //   error,
-  //   searchBooks,
-  // };
-
-  const contextValue = {
-    books: [],
-    error: "",
+  const contextValue: IBookContext = {
+    books: books || [],
+    error,
     searchBooks,
   };
-  // console.log("contextValue", contextValue);
+
   return (
     <BooksContext.Provider value={contextValue}>
       {children}
