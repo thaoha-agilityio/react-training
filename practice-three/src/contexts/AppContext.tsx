fc820a41499a9c8804e31ee8cbb9ba3d47f7bd21
Thao Y Ha <thao.ha@asnet.com.vn>
2023-02-13 14:40:46 +0700

    Render categories in view

--- practice-three/src/contexts/AppContext.tsx
+++ practice-three/src/contexts/AppContext.tsx
@@ -1,11 +1,14 @@
 import React, { ReactNode } from "react";
 
 import { BooksProvider } from "./BooksContext";
+import { CategoriesProvider } from "./CategoriesContext";
 
 type AppProviderProps = {
   children: ReactNode;
 };
 
-export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
-  return <BooksProvider>{children}</BooksProvider>;
-};
+export const AppProvider: React.FC<AppProviderProps> = ({ children }) => (
+  <CategoriesProvider>
+    <BooksProvider>{children}</BooksProvider>;
+  </CategoriesProvider>
+);
