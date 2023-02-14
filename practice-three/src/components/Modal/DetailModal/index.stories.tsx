2b166722bef0beda1eb00f237a55c28fc4fef7ce
Thaoha11 <thaohaexo@gmail.com>
2023-02-14 23:33:52 +0700

    Update storybook for detailModal

--- practice-three/src/components/Modal/DetailModal/index.stories.tsx
+++ practice-three/src/components/Modal/DetailModal/index.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { useState } from "react";
 
 import DetailModal from ".";
 
@@ -7,15 +8,32 @@ import { book } from "../../../constants/mockData";
 export default {
   title: "DetailModal",
   component: DetailModal,
-  argTypes: {
-    book: {
-      defaultValue: book,
-    },
-  },
 } as ComponentMeta<typeof DetailModal>;
 
-const Template: ComponentStory<typeof DetailModal> = (args) => (
-  <DetailModal {...args} />
-);
+const Template: ComponentStory<typeof DetailModal> = (args) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const handleCloseModal = (): void => {
+    console.log("Closing modal");
+  };
+
+  const handleCloseByKeyboard = (event: KeyboardEvent): void => {
+    console.log(`Keyboard event: ${event.key}`);
+  };
+
+  const handleToggleTheme = (): void => {
+    setIsDarkTheme((prev) => !prev);
+  };
+
+  return (
+    <DetailModal
+      book={book}
+      onCloseModal={handleCloseModal}
+      onCloseByKeyboard={handleCloseByKeyboard}
+      onToggleTheme={handleToggleTheme}
+      isDarkTheme={isDarkTheme}
+    />
+  );
+};
 
 export const Default = Template.bind({});
