203f58cf49083f65b68a0d157b9f1e55be821c72
Thaoha11 <thaohaexo@gmail.com>
2023-02-15 01:10:27 +0700

    Remove import BookProvider

--- practice-three/src/components/Modal/FilterModal/__test__/index.test.tsx
+++ practice-three/src/components/Modal/FilterModal/__test__/index.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-import { BooksContext, BooksProvider, IBookContext } from "../../../../contexts/BooksContext";
+import { BooksContext, IBookContext } from "../../../../contexts/BooksContext";
 
 import FilterModal from "..";
 
