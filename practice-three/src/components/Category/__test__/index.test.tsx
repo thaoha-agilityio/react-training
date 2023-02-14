2bee671692802fe1b99ffe898c88f9d81ca1d4cb
Thaoha11 <thaohaexo@gmail.com>
2023-02-15 02:06:34 +0700

    Update unit test for category component

--- practice-three/src/components/Category/__test__/index.test.tsx
+++ practice-three/src/components/Category/__test__/index.test.tsx
@@ -1,20 +1,55 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-import Category from "..";
 import { category } from "../../../constants/mockData";
+
+import Category from "..";
+import { CategoriesContext, ICategoriesContext } from "../../../contexts/CategoriesContext";
 
 const mockProps = {
   category: category,
   onSelectCategory: jest.fn(),
 };
 
+const mockContextValue = {
+  selectedIds: ['1']
+}
+
 describe("testing Category component", () => {
-  test("Should match data for Category component", () => {
+  it("Should match data for Category component", () => {
     render(<Category {...mockProps} />);
 
     const category = screen.getByTestId("category");
 
     expect(category).toBeInTheDocument();
+
+  });
+
+  // it("Should call onSelectCategory when categoty is clicked", () => {
+
+  //   render(<Category {...mockProps} />);
+
+  //   const category = screen.getByTestId("category");
+  //   fireEvent.click(category);
+
+  //   expect(mockProps.onSelectCategory).toHaveBeenCalled();
+  // });
+
+  it('does not call onSelectCategory if category is already selected', () => {
+    render(
+      <CategoriesContext.Provider value={mockContextValue as unknown as ICategoriesContext}>
+        <Category
+          {...mockProps}
+        />
+      </CategoriesContext.Provider >
+    );
+
+    const categoryElement = screen.getByTestId('category');
+    fireEvent.click(categoryElement);
+
+    render(<Category {...mockProps} />);
+    fireEvent.click(categoryElement);
+
+    expect(mockProps.onSelectCategory).not.toHaveBeenCalled();
   });
 });
