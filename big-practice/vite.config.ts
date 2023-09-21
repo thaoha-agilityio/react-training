b9a6887d9ca4a8e117abef1ad7f5e68ea2b3912e
thaoha-agilityio <thao.ha@asnet.com.vn>
2023-09-21 14:15:55 +0700

    feat: config packages and setup project

--- big-practice/vite.config.ts
+++ big-practice/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react(), EnvironmentPlugin('all'), tsconfigPaths()],
 });
