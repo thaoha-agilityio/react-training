21e5426e69d292ab9179d787e94c945169367669
thaoha-agilityio <thao.ha@asnet.com.vn>
2023-09-22 08:56:14 +0700

    feat: custom avatar theme

---
+++ big-practice/src/themes/components/avatar.ts
@@ -0,0 +1,21 @@
+const avatarTheme = {
+  sizes: {
+    base: {
+      width: '285px',
+      height: '301px',
+    },
+    md: {
+      width: '423px',
+      height: '500px',
+    },
+    xs: { width: '105px', height: '105px' },
+  },
+
+  baseStyle: {
+    default: {
+      borderWidth: '10px',
+    },
+  },
+};
+
+export default avatarTheme;
