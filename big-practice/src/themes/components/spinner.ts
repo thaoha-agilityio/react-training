0fad5fed43410ec3b5a5107c971873b326130bdf
thao.ha <thao.ha@asnet.com.vn>
2023-09-25 03:16:00 -0700

    feat: custom input and spinner theme

---
+++ big-practice/src/themes/components/spinner.ts
@@ -0,0 +1,18 @@
+const spinnerTheme = {
+  baseStyle: {
+    color: 'gray.300',
+    mx: 'auto',
+  },
+  variants: {
+    primary: {
+      borderWidth: 2,
+      speed: '0.8s',
+    },
+    secondary: {
+      borderWidth: 4,
+      speed: '0.5s',
+    },
+  },
+};
+
+export default spinnerTheme;
--- big-practice/src/themes/index.ts
+++ big-practice/src/themes/index.ts
@@ -10,12 +10,18 @@ import fontWeights from './fontWeights';
 // Custom theme of Component
 import buttonTheme from './components/button';
 import cardTheme from './components/card';
+import inputTheme from './components/input';
+import formLabelTheme from './components/formLabel';
+import spinnerTheme from './components/spinner';
 
 const CHAKRA_THEME = {
   ...extendTheme({
     components: {
       Button: buttonTheme,
       Card: cardTheme,
+      Input: inputTheme,
+      FormLabel: formLabelTheme,
+      Spinner: spinnerTheme,
     },
     colors: colors,
     fontSizes: fontSizes,
