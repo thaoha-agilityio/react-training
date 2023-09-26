b7ebdc5ce6eb2848600e05e2fd0e1a8f85458109
thao.ha <thao.ha@asnet.com.vn>
2023-09-26 11:48:41 +0700

    feat: revert code

--- big-practice/src/App.tsx
+++ big-practice/src/App.tsx
@@ -1,15 +1,7 @@
-import { Heading, Text } from '@chakra-ui/react';
-
 const App = () => {
   return (
     <>
-      <Heading fontWeight='bolder'>Hello word</Heading>
-      <Text color={'red'} fontWeight='bolder'>
-        Hello word
-      </Text>
-      <Text fontWeight='medium'>Hello word</Text>
-      <Text fontWeight='normal'>Hello word</Text>
-      <Text fontWeight='light'>Hello word</Text>
+      <h1>Hello word</h1>
     </>
   );
 };
