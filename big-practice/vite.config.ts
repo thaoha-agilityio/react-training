7eea20f6156f89fb836c313060c409aba6f51efe
thao.ha <thao.ha@asnet.com.vn>
2023-10-16 18:10:53 +0700

    feat: fix robot

--- big-practice/vite.config.ts
+++ big-practice/vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import EnvironmentPlugin from 'vite-plugin-environment';
+import Sitemap from 'vite-plugin-sitemap';
 
+const names = ['', 'shop', 'add-product', 'edit-product', 'shopping-cart'];
+const dynamicRoutes = names.map((name) => `/${name}`);
+const envVariables = loadEnv('mock', process.cwd(), '');
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -11,5 +15,19 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react(), EnvironmentPlugin('all'), tsconfigPaths()],
+  plugins: [
+    react(),
+    EnvironmentPlugin('all'),
+    tsconfigPaths(),
+    Sitemap({ dynamicRoutes, hostname: envVariables.VITE_HOST_NAME }),
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
