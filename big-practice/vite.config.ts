51181f5ba37ed47b8d66c6e8435b10c8f21a1bf3
thao.ha <thao.ha@asnet.com.vn>
2023-10-16 23:02:26 +0700

    feat: config vercel

--- big-practice/vite.config.ts
+++ big-practice/vite.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import EnvironmentPlugin from 'vite-plugin-environment';
-import Sitemap from 'vite-plugin-sitemap';
 
-const names = ['', 'shop', 'add-product', 'edit-product', 'shopping-cart'];
-const dynamicRoutes = names.map((name) => `/${name}`);
-const envVariables = loadEnv('mock', process.cwd(), '');
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -15,19 +11,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [
-    react(),
-    EnvironmentPlugin('all'),
-    tsconfigPaths(),
-    Sitemap({ dynamicRoutes, hostname: envVariables.VITE_HOST_NAME }),
-  ],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
+  plugins: [react(), EnvironmentPlugin('all'), tsconfigPaths()],
 });
