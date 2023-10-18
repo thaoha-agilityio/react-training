33a21bb58526e6f57da92f8d1e13e59e15d58623
thao.ha <thao.ha@asnet.com.vn>
2023-10-19 00:20:14 +0700

    feat: update header component

--- big-practice/src/components/Header/index.tsx
+++ big-practice/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { shallow } from 'zustand/shallow';
 import { memo } from 'react';
 import { Box, Container, Flex, HStack, Heading } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
@@ -11,41 +12,47 @@ import { MENU } from '@constants';
 
 // Components
 import Menu from '@components/Menu';
+import ShoppingCart from '@components/ShoppingCart';
 
-import ShoppingCart from './ShoppingCart';
+// Stores
+import { useCartStore } from '@stores';
 
-const Header = (): JSX.Element => (
-  <Box as='header'>
-    <Container maxW='container.xl'>
-      <Flex justifyContent='space-between' alignItems='center' py='30px'>
-        <Flex>
-          <Link to='/'>
-            <HStack spacing='5px'>
-              <FurniroLogo />
-              <Heading as='h1' fontSize={{ base: 'xxl', md: '2xl' }}>
-                Furniro
-              </Heading>
-            </HStack>
-          </Link>
+const Header = (): JSX.Element => {
+  const [cart] = useCartStore((state) => [state.cart], shallow);
+
+  return (
+    <Box as='header'>
+      <Container maxW='container.xl'>
+        <Flex justifyContent='space-between' alignItems='center' py='30px'>
+          <Flex>
+            <Link to='/'>
+              <HStack spacing='5px'>
+                <FurniroLogo />
+                <Heading as='h1' fontSize={{ base: 'xxl', md: '2xl' }}>
+                  Furniro
+                </Heading>
+              </HStack>
+            </Link>
+          </Flex>
+
+          <Menu menu={MENU} />
+
+          <HStack spacing={{ base: '15px', md: '45px' }} display={{ base: 'none', md: 'flex' }}>
+            <Link to='/' aria-label='Account'>
+              <AccountIcon />
+            </Link>
+            <Link to='/' aria-label='Search'>
+              <SearchIcon />
+            </Link>
+            <Link to='/' aria-label='Heart'>
+              <HeartIcon />
+            </Link>
+            <ShoppingCart numberOfItems={cart.length} />
+          </HStack>
         </Flex>
-
-        <Menu menu={MENU} />
-
-        <HStack spacing={{ base: '15px', md: '45px' }} display={{ base: 'none', md: 'flex' }}>
-          <Link to='/' aria-label='Account'>
-            <AccountIcon />
-          </Link>
-          <Link to='/' aria-label='Search'>
-            <SearchIcon />
-          </Link>
-          <Link to='/' aria-label='Heart'>
-            <HeartIcon />
-          </Link>
-          <ShoppingCart />
-        </HStack>
-      </Flex>
-    </Container>
-  </Box>
-);
+      </Container>
+    </Box>
+  );
+};
 
 export default memo(Header);
--- big-practice/src/components/Menu/__test__/Menu.test.tsx
+++ big-practice/src/components/Menu/__test__/Menu.test.tsx
@@ -1,34 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
-
 import Menu from '../index';
+
+// Constants
 import { MENU } from '@constants/menu';
+
+// Helpers
+import { renderWithRouterAndQuery } from '@helpers';
 
 describe('Menu component', () => {
   it('renders a list of menu items with links', () => {
-    render(
-      <Router>
-        <Menu menu={MENU} />
-      </Router>,
-    );
+    const { container, getByRole } = renderWithRouterAndQuery(<Menu menu={MENU} />);
+
+    expect(container).toBeInTheDocument();
 
     // Check that each menu item and link is rendered
     MENU.forEach((item) => {
-      const linkElement = screen.getByRole('link', { name: item.title });
+      const linkElement = getByRole('link', { name: item.title });
       expect(linkElement).toBeInTheDocument();
       expect(linkElement).toHaveAttribute('href', item.path);
     });
-  });
-
-  it('renders the correct number of menu items', () => {
-    render(
-      <Router>
-        <Menu menu={MENU} />
-      </Router>,
-    );
-
-    // Check that the correct number of menu items is rendered
-    const listItems = screen.getAllByRole('listitem');
-    expect(listItems).toHaveLength(MENU.length);
   });
 });
