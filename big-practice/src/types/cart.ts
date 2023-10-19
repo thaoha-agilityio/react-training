34d999b98f24a489994ca148bc5db508e2890aa4
thao.ha <thao.ha@asnet.com.vn>
2023-10-19 09:30:21 +0700

    feat: updaate cart item props

--- big-practice/src/types/cart.ts
+++ big-practice/src/types/cart.ts
@@ -1,5 +1,10 @@
+import { IProduct } from './product';
+
 // Types for cart
 export interface ICart {
   productId: string;
   quantity: number;
 }
+
+// Types for product in cart
+export interface IProductCart extends IProduct, Omit<ICart, 'productId'> {}
