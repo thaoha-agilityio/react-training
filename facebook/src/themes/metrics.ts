f1a57048a83cecc1f7fb0403b35abf2738d5e4b1
Thao Ha <thao.ha@asnet.com.vn>
2024-04-26 17:26:55 +0700

    feat: define color theme

---
+++ facebook/src/themes/metrics.ts
@@ -0,0 +1,39 @@
+export const fonts = {
+  heading: `Poppins, sans-serif`,
+  body: `Poppins, sans-serif`,
+};
+
+export const fontSizes = {
+  xs: '11px',
+  tiny: '12px',
+  sm: '13px',
+  base: '14px',
+  md: '15px',
+  lg: '18px',
+  xl: '20px',
+  xxl: '28px',
+  '2xl': '32px',
+};
+
+export const fontWeights = {
+  light: 300,
+  normal: 400,
+  medium: 500,
+  bold: 600,
+  bolder: 700,
+};
+
+export const lineHeights = {
+  xs: '15px',
+  sm: '17px',
+  md: '21px',
+  lg: '27px',
+  xl: '37px',
+};
+
+export const radius = {
+  xs: '5px',
+  sm: '6px',
+  md: '8px',
+  lg: '20px',
+};
