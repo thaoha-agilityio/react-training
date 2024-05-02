99dbc120c40c7ef009cf5e22103473a48bb3ce98
Thao Ha <thao.ha@asnet.com.vn>
2024-05-02 09:27:44 +0700

    feat: update naming convention

--- facebook/src/themes/metrics.ts
+++ facebook/src/themes/metrics.ts
@@ -7,15 +7,15 @@ export const fontSizes = {
   lg: "18px",
   xl: "20px",
   xxl: "28px",
-  "2xl": "32px",
+  "3xl": "32px",
 };
 
 export const fontWeights = {
   light: 300,
   normal: 400,
   medium: 500,
-  bold: 600,
-  bolder: 700,
+  semiBold: 600,
+  bold: 700,
 };
 
 export const lineHeights = {
