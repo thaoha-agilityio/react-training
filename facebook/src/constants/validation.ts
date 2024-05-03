835f24d0e722a43f9cd904dee7b97aa4b9af90a7
Thao Ha <thao.ha@asnet.com.vn>
2024-05-03 14:17:57 +0700

    feat: update SignInForm

--- facebook/src/constants/validation.ts
+++ facebook/src/constants/validation.ts
@@ -1,8 +1,8 @@
 export const ERROR_MESSAGES = {
-  FIELD_REQUIRED: (fieldName: string) => `${fieldName} is required`,
-  EMAIL_INVALID: 'Invalid email format',
+  FIELD_REQUIRED: (fieldName: string) => `${fieldName} is required.`,
+  EMAIL_INVALID: 'Invalid email format.',
   PASSWORD_NOT_LONG: 'Your password must be at least 10 characters long.',
   PASSWORD_NOT_HAVE_NUMBER: 'Your password must contain at least one number.',
   PASSWORD_NOT_HAVE_SYMBOL: 'Your password must contain at least one special character.',
-  EMAIL_IS_INVALID: 'Invalid email or password',
+  EMAIL_IS_INVALID: 'Invalid email or password.',
 };
