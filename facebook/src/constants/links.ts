473ef488d372a664c416ab0f7f30ae852ee466ee
Thao Ha <thao.ha@asnet.com.vn>
2024-05-06 10:52:40 +0700

    fix: reslove conflicts

--- facebook/src/constants/links.ts
+++ facebook/src/constants/links.ts
@@ -1,4 +1,180 @@
-import { VideoIcon, HomeIcon, FriendIcon, MarketPlaceIcon, GroupIcon, MemoriesIcon, SaveIcon, PageIcon, EventIcon } from '@/components/Icons';
+import {
+  VideoIcon,
+  HomeIcon,
+  FriendIcon,
+  MarketPlaceIcon,
+  GroupIcon,
+  MemoriesIcon,
+  SaveIcon,
+  PageIcon,
+  EventIcon,
+} from '@/components/Icons';
+
+export const LOCATION_LINKS = [
+  {
+    href: '#',
+    label: 'English (UK)',
+  },
+  {
+    href: '#',
+    label: 'Tiếng Việt',
+  },
+  {
+    href: '#',
+    label: '中文(台灣',
+  },
+  {
+    href: '#',
+    label: '한국어',
+  },
+  {
+    href: '#',
+    label: '日本語',
+  },
+  {
+    href: '#',
+    label: 'Français (France)',
+  },
+  {
+    href: '#',
+    label: 'ภาษาไทย',
+  },
+  {
+    href: '#',
+    label: 'Español',
+  },
+  {
+    href: '#',
+    label: 'Português (Brasil)',
+  },
+  {
+    href: '#',
+    label: 'Deutsch',
+  },
+  {
+    href: '#',
+    label: 'Italiano',
+  },
+];
+
+export const FOOTER_LINKS = [
+  {
+    href: '#',
+    label: 'Sign Up',
+  },
+  {
+    href: '#',
+    label: 'Log in',
+  },
+  {
+    href: '#',
+    label: 'Messenger',
+  },
+  {
+    href: '#',
+    label: 'Facebook Lite',
+  },
+  {
+    href: '#',
+    label: 'Video',
+  },
+  {
+    href: '#',
+    label: 'Places',
+  },
+  {
+    href: '#',
+    label: 'Games',
+  },
+  {
+    href: '#',
+    label: 'Marketplace',
+  },
+  {
+    href: '#',
+    label: 'Meta Pay',
+  },
+  {
+    href: '#',
+    label: 'Meta Store',
+  },
+  {
+    href: '#',
+    label: 'Meta Quest',
+  },
+  {
+    href: '#',
+    label: 'Meta AI',
+  },
+  {
+    href: '#',
+    label: 'Instagram',
+  },
+  {
+    href: '#',
+    label: 'Threads',
+  },
+  {
+    href: '#',
+    label: 'Fundraisers',
+  },
+  {
+    href: '#',
+    label: 'Services',
+  },
+  {
+    href: '#',
+    label: 'Voting Information Centre',
+  },
+  {
+    href: '#',
+    label: 'Privacy Policy',
+  },
+  {
+    href: '#',
+    label: 'Privacy Centre',
+  },
+  {
+    href: '#',
+    label: 'Groups',
+  },
+  {
+    href: '#',
+    label: 'About',
+  },
+  {
+    href: '#',
+    label: 'Create ad',
+  },
+  {
+    href: '#',
+    label: 'Create Page',
+  },
+  {
+    href: '#',
+    label: 'Developers',
+  },
+  {
+    href: '#',
+    label: 'Careers',
+  },
+  {
+    href: '#',
+    label: 'Cookies',
+  },
+  {
+    href: '#',
+    label: 'Terms',
+  },
+  {
+    href: '#',
+    label: 'Help',
+  },
+  {
+    href: '#',
+    label: 'Contact uploading and non-usersSettings',
+  },
+];
 
 export const ICON_LINKS = [
   {
@@ -21,31 +197,31 @@ export const ICON_LINKS = [
 
 export const SIDEBAR_LINKS = [
   {
-    label:'Groups',
+    label: 'Groups',
     icon: GroupIcon,
     href: '#',
   },
   {
-    label:'Memories',
+    label: 'Memories',
     icon: MemoriesIcon,
     href: '#',
   },
   {
-    label:'Saved',
+    label: 'Saved',
     icon: SaveIcon,
     href: '#',
   },
   {
-    label:'Pages',
+    label: 'Pages',
     icon: PageIcon,
     href: '#',
   },
   {
-    label:'Events',
+    label: 'Events',
     icon: EventIcon,
     href: '#',
   },
-]
+];
 
 export const FOOTER_SIDEBAR_LINKS = [
   {
@@ -64,16 +240,9 @@ export const FOOTER_SIDEBAR_LINKS = [
     href: '#',
     label: 'Add Choices',
   },
-  {
-    href: '#',
-    label: 'Cookies',
-  },
-  {
-    href: '#',
-    label: 'More',
-  },
+  { href: '#', label: 'More' },
   {
     href: '#',
     label: 'Meta © 2024',
   },
-]
+];
