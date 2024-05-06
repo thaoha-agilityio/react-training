490e264e1a84b679a6b5a939511f2e4c0b721c8a
Thao Ha <thao.ha@asnet.com.vn>
2024-05-06 16:07:28 +0700

    feat: implement ViewPostModal component

---
+++ facebook/src/components/Modal/PostModal.tsx/index.tsx
@@ -0,0 +1,51 @@
+import { Avatar, Box, Flex, IconButton, Stack, Textarea } from '@chakra-ui/react';
+
+// Components
+import { Comment, CustomModal, Post } from '@/components';
+import { SendIcon } from '@/components/Icons';
+
+// Constants
+import { DEFAULT_IMAGE, INPUT_PLACEHOLDER } from '@/constants';
+
+// Types
+import { IComment, IPost } from '@/types';
+
+interface PostModalProps {
+  isOpen: boolean;
+  post: IPost;
+  comments: IComment[];
+  onClose: () => void;
+}
+
+const PostModal = ({ isOpen, onClose, comments, post }: PostModalProps) => {
+  return (
+    <CustomModal isOpen={isOpen} onClose={onClose} title={post.author} size='2xl'>
+      <Post post={post} isModal />
+
+      {/* List comment */}
+      <Stack spacing='10px' pl='10px'>
+        {comments.map((comment: IComment) => (
+          <Comment comment={comment} key={comment.id} />
+        ))}
+      </Stack>
+
+      {/* Write comment */}
+      <Flex as='form' p='10px' gap='10px'>
+        <Avatar src={DEFAULT_IMAGE} />
+        <Box pos='relative' w='full'>
+          <Textarea variant='filled' placeholder={INPUT_PLACEHOLDER.COMMENT} />
+          <Box pos='absolute' right={0} top='35px'>
+            <IconButton
+              type='submit'
+              icon={<SendIcon />}
+              aria-label='comment-icon'
+              variant='action'
+            />
+          </Box>
+        </Box>
+      </Flex>
+    </CustomModal>
+  );
+};
+
+export default PostModal;
--- facebook/src/components/Post/index.tsx
+++ facebook/src/components/Post/index.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react';
-import { Box, Text, Image, Stack, Container, Flex, Button } from '@chakra-ui/react';
+import { Box, Text, Image, Stack, Flex, Button, Divider } from '@chakra-ui/react';
 
 // Components
 import { UserProfile } from '@/components';
@@ -8,49 +8,55 @@ import { CommentIcon, LikeIcon, ShareIcon } from '../Icons';
 // Constants
 import { PLACEHOLDER_IMAGE } from '@/constants';
 
+// Types
+import { IPost } from '@/types';
+
 // TODO: update props later
 interface PostProps {
-  userName: string;
-  content: string;
-  image: string;
+  post: IPost;
+  isModal?: boolean;
 }
 
-const Post = memo(({ userName, content, image }: PostProps) => {
+const Post = memo(({ post, isModal = false }: PostProps) => {
+  const { author, content, image } = post || {};
+
   return (
-    <Container>
-      <Stack
-        spacing='15px'
-        py='10px'
-        borderRadius='md'
-        boxShadow='0 2px 4px rgba(0, 0, 0, .1), 0 8px 16px rgba(0, 0, 0, .1)'
-      >
-        <Box pl='15px'>
-          <UserProfile userName={userName} />
-        </Box>
-        <Text pl='15px'>{content}</Text>
-        <Box w='618px' h='533px'>
-          <Image
-            src={image}
-            alt='post-img'
-            width='100%'
-            height='100%'
-            objectFit='cover'
-            fallbackSrc={PLACEHOLDER_IMAGE}
-          />
-        </Box>
-        <Flex justifyContent='space-between' px='10px'>
-          <Button variant='unstyled' leftIcon={<LikeIcon />} alignItems='center' w='150px'>
-            Like
-          </Button>
-          <Button variant='unstyled' leftIcon={<CommentIcon />} alignItems='center' w='150px'>
-            Comment
-          </Button>
-          <Button variant='unstyled' leftIcon={<ShareIcon />} alignItems='center' w='150px'>
-            Share
-          </Button>
-        </Flex>
-      </Stack>
-    </Container>
+    <Stack
+      spacing='15px'
+      py='10px'
+      borderRadius='md'
+      width='full'
+      boxShadow={isModal ? 'none' : '0 2px 4px rgba(0, 0, 0, .1), 0 8px 16px rgba(0, 0, 0, .1)'}
+    >
+      <Box pl='15px'>
+        {/* TODO: will handle get userName by author */}
+        <UserProfile userName={author} />
+      </Box>
+      <Text pl='15px'>{content}</Text>
+      <Box w='full' h='533px'>
+        <Image
+          src={image}
+          alt='post-img'
+          width='100%'
+          height='100%'
+          objectFit='cover'
+          fallbackSrc={PLACEHOLDER_IMAGE}
+        />
+      </Box>
+
+      <Flex justifyContent='space-between' px='10px'>
+        <Button variant='unstyled' leftIcon={<LikeIcon />} alignItems='center' w='150px'>
+          Like
+        </Button>
+        <Button variant='unstyled' leftIcon={<CommentIcon />} alignItems='center' w='150px'>
+          Comment
+        </Button>
+        <Button variant='unstyled' leftIcon={<ShareIcon />} alignItems='center' w='150px'>
+          Share
+        </Button>
+      </Flex>
+      <Divider />
+    </Stack>
   );
 });
 
