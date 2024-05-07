5904c828f324de98e701ceeadf191d589b1b6c94
Thao Ha <thao.ha@asnet.com.vn>
2024-05-07 17:38:26 +0700

    feat: update config vite

--- facebook/vite.config.ts
+++ facebook/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import EnvironmentPlugin from 'vite-plugin-environment';
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin('all')],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
