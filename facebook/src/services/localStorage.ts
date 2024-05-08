7c32ce090427cd8b8fd0715cb64fa980d5d75e0f
Thao Ha <thao.ha@asnet.com.vn>
2024-05-08 11:00:50 +0700

    feat: encrypt security data

--- facebook/src/services/localStorage.ts
+++ facebook/src/services/localStorage.ts
@@ -1,8 +1,10 @@
+import { decryptAccessToken, encryptAccessToken } from '@/utils';
+
 export const getItemLocalStorage = (key: string) => {
   const localStorageData = localStorage.getItem(key);
 
-  return localStorageData ? JSON.parse(localStorageData) : null;
+  return localStorageData ? decryptAccessToken(JSON.parse(localStorageData)) : null;
 };
 
 export const setItemLocalStorage = <T>(key: string, data: T) =>
-  localStorage.setItem(key, JSON.stringify(data));
+  localStorage.setItem(key, encryptAccessToken(JSON.stringify(data)));
