0aa0b4149ca462a08d2b40fd50ed1b1d2714c136
Thao Ha <thao.ha@asnet.com.vn>
2024-05-09 14:56:50 +0700

    feat: handle logout

--- facebook/src/components/Header/index.tsx
+++ facebook/src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
 import {
   Container,
   Flex,
@@ -10,59 +12,67 @@ import {
 } from '@chakra-ui/react';
 
 // Components
-import {
-  ChevronDownIcon,
-  FacebookIcon,
-  MenuIcon,
-  MessageIcon,
-  NotificationIcon,
-  SearchIcon,
-} from '../Icons';
+import { FacebookIcon, MenuIcon, MessageIcon, NotificationIcon, SearchIcon } from '../Icons';
+import Logout from '../Logout';
 
 // Constants
-import { ICON_LINKS } from '@/constants';
+import { ICON_LINKS, ROUTES } from '@/constants';
 
-const Header = () => (
-  <HStack
-    as='header'
-    px='15px'
-    py='10px'
-    boxShadow='0 2px 4px rgba(0, 0, 0, .1), 0 8px 16px rgba(0, 0, 0, .1)'
-  >
-    <Flex alignItems='center' gap='15px'>
-      <Link href='/'>
-        <FacebookIcon />
-      </Link>
-      <InputGroup>
-        <InputLeftElement pointerEvents='none' top='-3px'>
-          <SearchIcon />
-        </InputLeftElement>
-        <Input variant='filled' h='40px' w='212px' placeholder='Search Facebook' />
-      </InputGroup>
-    </Flex>
+// Stores
+import { useAuthStore } from '@/stores';
 
-    <Container>
-      <Flex justifyContent='space-between'>
-        {ICON_LINKS.map((link, index) => {
-          const { href, icon } = link || {};
-          const LinkIcon = icon;
+const Header = () => {
+  // Auth store
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const navigate = useNavigate();
 
-          return (
-            <Link key={`${index}-${href}`} href={href}>
-              <LinkIcon />
-            </Link>
-          );
-        })}
+  const handleLogout = useCallback(() => {
+    clearAuth();
+    navigate(ROUTES.SIGN_IN);
+  }, [clearAuth, navigate]);
+
+  return (
+    <HStack
+      as='header'
+      px='15px'
+      py='10px'
+      boxShadow='0 2px 4px rgba(0, 0, 0, .1), 0 8px 16px rgba(0, 0, 0, .1)'
+    >
+      <Flex alignItems='center' gap='15px'>
+        <Link href='/'>
+          <FacebookIcon />
+        </Link>
+        <InputGroup>
+          <InputLeftElement pointerEvents='none' top='-3px'>
+            <SearchIcon />
+          </InputLeftElement>
+          <Input variant='filled' h='40px' w='212px' placeholder='Search Facebook' />
+        </InputGroup>
       </Flex>
-    </Container>
 
-    <Flex justifyContent='space-between' gap='10px'>
-      <IconButton aria-label='menu-btn' icon={<MenuIcon />} variant='icon' />
-      <IconButton aria-label='message-btn' icon={<MessageIcon />} variant='icon' />
-      <IconButton aria-label='notify-btn' icon={<NotificationIcon />} variant='icon' />
-      <IconButton aria-label='menu-btn' icon={<ChevronDownIcon />} variant='icon' />
-    </Flex>
-  </HStack>
-);
+      <Container>
+        <Flex justifyContent='space-between'>
+          {ICON_LINKS.map((link, index) => {
+            const { href, icon } = link || {};
+            const LinkIcon = icon;
+
+            return (
+              <Link key={`${index}-${href}`} href={href}>
+                <LinkIcon />
+              </Link>
+            );
+          })}
+        </Flex>
+      </Container>
+
+      <Flex justifyContent='space-between' gap='10px'>
+        <IconButton aria-label='menu-btn' icon={<MenuIcon />} variant='icon' />
+        <IconButton aria-label='message-btn' icon={<MessageIcon />} variant='icon' />
+        <IconButton aria-label='notify-btn' icon={<NotificationIcon />} variant='icon' />
+        <Logout onLogout={handleLogout} />
+      </Flex>
+    </HStack>
+  );
+};
 
 export default Header;
