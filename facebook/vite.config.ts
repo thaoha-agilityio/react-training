8af4a6c7b015d05a521a7e7fe2861981fc26a7c4
thao.ha <thao.ha@asnet.com.vn>
2024-05-12 16:24:14 +0700

    feat: config env

--- facebook/vite.config.ts
+++ facebook/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+const envVariables = loadEnv('mock', process.cwd(), '');
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+
+  define: {
+    'process.env.SOME_KEY': JSON.stringify(envVariables.SOME_KEY),
   },
 });
