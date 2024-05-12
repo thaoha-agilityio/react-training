384304035db4332bb74cd5f0e5cdec97edd1bc2e
thao.ha <thao.ha@asnet.com.vn>
2024-05-12 14:55:17 +0700

    feat: Write unit test for SignIn component

---
+++ facebook/src/components/SignInForm/__test__/SignInForm.test.tsx
@@ -0,0 +1,86 @@
+import { act, fireEvent, renderWithRouterAndQuery, waitFor } from '@/utils';
+// Components
+import SignInForm from '..';
+import SignUpFormModal from '@/components/Modal/SignUpModal';
+
+// Constants
+import { ERROR_MESSAGES, INPUT_PLACEHOLDER, STATUS } from '@/constants';
+// Hooks
+import * as hooks from '@/hooks';
+
+// Services
+import { api } from '@/services';
+
+// Mocks
+import { USERS, USER_INVALID } from '@/mocks';
+
+describe('SignInForm Component', () => {
+  const setup = () => {
+    const { getByPlaceholderText, getByText } = renderWithRouterAndQuery(<SignInForm />);
+
+    const email = getByPlaceholderText(INPUT_PLACEHOLDER.EMAIL);
+    const password = getByPlaceholderText(INPUT_PLACEHOLDER.PASSWORD);
+
+    const submitBtn = getByText('Log In');
+    const sigUpBtn = getByText('Create New Account');
+
+    return {
+      email,
+      password,
+      submitBtn,
+      sigUpBtn,
+    };
+  };
+
+  it('Should render SignInForm Component correctly', () => {
+    const component = renderWithRouterAndQuery(<SignInForm />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should create the Employee with valid data', () => {
+    jest.spyOn(api, 'postData').mockResolvedValue(USERS[0]);
+
+    const { email, password, submitBtn } = setup();
+    act(() => {
+      fireEvent.change(email, { target: { value: USERS[0].email } });
+      fireEvent.change(password, { target: { value: USERS[0].password } });
+
+      fireEvent.click(submitBtn);
+    });
+  });
+
+  it('should create the Employee with invalid data', () => {
+    jest.spyOn(api, 'postData').mockRejectedValue(new Error('Error'));
+
+    const mockShowToast = jest.fn();
+    (jest.spyOn(hooks, 'useCustomToast') as jest.Mock).mockReturnValue({
+      showToast: mockShowToast,
+    });
+
+    const { email, password, submitBtn } = setup();
+    act(() => {
+      fireEvent.change(email, { target: { value: USER_INVALID.email } });
+      fireEvent.change(password, { target: { value: USER_INVALID.password } });
+
+      fireEvent.click(submitBtn);
+    });
+
+    waitFor(() => {
+      expect(mockShowToast).toHaveBeenCalledWith(STATUS.ERROR, ERROR_MESSAGES.DEFAULT_API_ERROR);
+    });
+  });
+
+  it('Should be show SignUpModal', () => {
+    const { sigUpBtn } = setup();
+
+    fireEvent.click(sigUpBtn);
+
+    const onClose = jest.fn();
+    const { getByText } = renderWithRouterAndQuery(
+      <SignUpFormModal isOpen={true} onClose={onClose} />,
+    );
+
+    expect(getByText('Sign up')).toBeInTheDocument();
+  });
+});
---
+++ facebook/src/mocks/users.ts
@@ -0,0 +1,11 @@
+export const USERS = [
+  {
+    email: 'tester@gmail.com',
+    password: 'Tester123@',
+  },
+];
+
+export const USER_INVALID = {
+  email: 'tester.gmail.com',
+  password: 'Tester123@',
+};
