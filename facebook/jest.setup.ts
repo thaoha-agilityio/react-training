dc03d28ad03731219f357bf94f0e08642dd779c0
Thao Ha <thao.ha@asnet.com.vn>
2024-05-16 16:39:29 +0700

    faet: update package

--- facebook/jest.setup.ts
+++ facebook/jest.setup.ts
@@ -11,13 +11,3 @@ jest.mock('@/hooks', () => ({
   __esModule: true,
   ...jest.requireActual('@/hooks'),
 }));
-
-// Mock react query
-jest.mock('@tanstack/react-query', () => ({
-  useQuery: jest.fn(),
-}));
-
-jest.mock('@/services/APIRequest', () => ({
-  __esModule: true,
-  ...jest.requireActual('@/services/APIRequest'),
-}));
