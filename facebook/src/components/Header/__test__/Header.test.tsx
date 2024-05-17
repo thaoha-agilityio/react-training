7a8f3c808cbc300b84280be29bc4042f15d543d5
Thao Ha <thao.ha@asnet.com.vn>
2024-05-17 17:53:19 +0700

    feat: update unit test for Header component

--- facebook/src/components/Header/__test__/Header.test.tsx
+++ facebook/src/components/Header/__test__/Header.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@/utils';
+import { renderWithRouterAndQuery } from '@/utils';
 
 import Header from '..';
 
 describe('Header Component', () => {
   it('Should render Header Component correctly', () => {
-    const component = render(<Header />);
+    const component = renderWithRouterAndQuery(<Header />);
 
     expect(component).toMatchSnapshot();
   });
