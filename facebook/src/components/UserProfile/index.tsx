1b9eb3628d88d8ed63615ed7ca3a53212f16ae76
thao.ha <thao.ha@asnet.com.vn>
2024-05-19 15:34:55 +0700

    feat: update UI for homepage

--- facebook/src/components/UserProfile/index.tsx
+++ facebook/src/components/UserProfile/index.tsx
@@ -10,7 +10,7 @@ interface UserProfileProps {
 const UserProfile = memo(({ userName }: UserProfileProps) => (
   <Flex gap='10px' alignItems='center'>
     <Avatar src={DEFAULT_IMAGE} name='avatar' />
-    <Text>{userName}</Text>
+    <Text fontWeight='semibold'>{userName}</Text>
   </Flex>
 ));
 
