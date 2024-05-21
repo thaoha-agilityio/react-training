107cfb713b258d8805d77b746e21b74e83111ec7
Thao Ha <thao.ha@asnet.com.vn>
2024-05-21 14:38:03 +0700

    feat: install snapshot

--- facebook/src/mocks/comments.ts
+++ facebook/src/mocks/comments.ts
@@ -3,7 +3,8 @@ import { IComment } from '@/types';
 export const COMMENTS: IComment[] = [
   {
     id: 1,
-    author: 1,
+    authorId: 1,
+    authorName: 'Pam',
     postId: 2,
     content: 'Comment1',
     likes: [2],
