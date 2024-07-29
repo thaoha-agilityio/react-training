2eaf8a812fb8e770422fd1c99cc5284a1839add9
Thao Ha <thao.ha@asnet.com.vn>
2024-07-29 16:33:40 +0700

    feat: add size for icon component

--- project/src/components/Icons/DashboardIcon.tsx
+++ project/src/components/Icons/DashboardIcon.tsx
@@ -1,7 +1,13 @@
 import { SVGProps } from "react";
 
 export const DashboardIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" {...props}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 448 512"
+    width={22}
+    height={22}
+    {...props}
+  >
     <path d="M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6 25.3V268.4H0v149.9zm203.8 28L448 480V268.4H203.8v177.9zm0-380.6v180.1H448V32L203.8 65.7z" />
   </svg>
 );
