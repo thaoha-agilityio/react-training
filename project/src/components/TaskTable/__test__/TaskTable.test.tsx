4dd03d3067a35df3f5d8fd83ae3df84ba362db38
Thao Ha <thao.ha@asnet.com.vn>
2024-08-07 15:18:32 +0700

    feat: update unit testing

--- project/src/components/TaskTable/__test__/TaskTable.test.tsx
+++ project/src/components/TaskTable/__test__/TaskTable.test.tsx
@@ -6,26 +6,11 @@ import TaskTable from "..";
 // Mocks
 import { TASKS } from "@/mocks";
 
-// Constants
-import { TASK_STATUS } from "@/constants";
-
 describe("TaskTable component", () => {
   it("should render the TaskTable correctly", () => {
     const { container } = render(<TaskTable tasks={TASKS} />);
 
     expect(container).toMatchSnapshot();
-  });
-
-  it("should renders correct styles for task status", () => {
-    const { getByText } = render(<TaskTable tasks={TASKS} />);
-
-    // Check if the correct classes are applied based on task status
-    expect(getByText(TASK_STATUS.DONE).closest("td")).toHaveClass(
-      "text-green-600",
-    );
-    expect(getByText(TASK_STATUS.IN_PROGRESS).closest("td")).toHaveClass(
-      "text-indigo-600",
-    );
   });
 
   it("should show edit modal when edit button is clicked", async () => {
