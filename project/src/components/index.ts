9c004edf719ad2eed578597c06fbd08c99e27680
Thao Y Ha <thao.ha@asnet.com.vn>
2024-08-13 14:02:17 +0700

    Feat/implement breadcrumbs component

--- project/src/components/index.ts
+++ project/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Avatar } from "./Avatar";
+export { default as Breadcrumb } from "./Breadcrumbs";
 export { default as Button } from "./Button";
 export { default as Dropdown } from "./Dropdown";
 export { default as ErrorBoundary } from "./ErrorBoundary";
