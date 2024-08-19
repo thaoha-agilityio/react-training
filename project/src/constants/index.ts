8ea9b17f6f7ef2a6dacbf1fce571170292c8d6c8
Thao Y Ha <thao.ha@asnet.com.vn>
2024-08-19 16:44:26 +0700

    feat: handle show toast message

--- project/src/constants/index.ts
+++ project/src/constants/index.ts
@@ -6,4 +6,5 @@ export * from "./pagination";
 export * from "./routes";
 export * from "./status";
 export * from "./taskTable";
+export * from "./toast";
 export * from "./validation";
---
+++ project/src/constants/toast.ts
@@ -0,0 +1,18 @@
+// Time in milliseconds
+export const TOAST_AUTO_CLOSE = 1000;
+
+export enum TOAST_POSITION {
+  TOP_RIGHT = "top-right",
+  TOP_CENTER = "top-center",
+  TOP_LEFT = "top-left",
+  BOTTOM_RIGHT = "bottom-right",
+  BOTTOM_CENTER = "bottom-center",
+  BOTTOM_LEFT = "bottom-left",
+}
+
+export enum TOAST_STATUS {
+  SUCCESS = "success",
+  ERROR = "error",
+  WARNING = "warning",
+  INFO = "info",
+}
