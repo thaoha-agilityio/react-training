082d8d502a97715b27cb0ba9a51c4fe57ae64feb
Thao Ha <thao.ha@asnet.com.vn>
2024-08-21 09:38:54 +0700

    feat: update unit testing for page

--- project/src/pages/Home/__test__/Home.test.tsx
+++ project/src/pages/Home/__test__/Home.test.tsx
@@ -1,9 +1,30 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
+// Pages
 import Home from "..";
 
+// Services
+import { api } from "@/services";
+
+// Mocks
+import { TASKS } from "@/mocks";
+
+// Hooks
+import * as hooks from "@/hooks";
+
 describe("Home page", () => {
+  beforeEach(() => {
+    jest.spyOn(api, "getData").mockResolvedValue({ data: TASKS, total: "4" });
+
+    (jest.spyOn(hooks, "useTaskPagination") as jest.Mock).mockImplementation(
+      () => ({
+        data: TASKS,
+        isLoading: false,
+        fetchAtPage: jest.fn(),
+      }),
+    );
+  });
   it("should render the Home correctly", () => {
     const { container } = render(
       <MemoryRouter>
@@ -12,5 +33,15 @@ describe("Home page", () => {
     );
 
     expect(container).toMatchSnapshot();
+  });
+
+  it("should render TaskForm when click add button", () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+
+    fireEvent.click(getByText("+ Add Tasks"));
   });
 });
