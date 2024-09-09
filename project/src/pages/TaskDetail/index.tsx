7cee1e587d1e22cb49ed1d659c112fb0054e197c
Thao Ha <thao.ha@asnet.com.vn>
2024-09-09 17:19:15 +0700

    feat: handle go back for home

--- project/src/pages/TaskDetail/index.tsx
+++ project/src/pages/TaskDetail/index.tsx
@@ -1,7 +1,7 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 // Components
-import { Breadcrumbs, TaskDetail } from "@/components";
+import { Breadcrumbs, Button, TaskDetail } from "@/components";
 import { SpinnerIcon } from "@/components/Icons";
 
 // Constants
@@ -12,6 +12,8 @@ import { useTaskGetDetail } from "@/hooks";
 
 const TaskDetailPage = () => {
   const { id = "" } = useParams();
+
+  const navigate = useNavigate();
 
   const DETAIL_TASK_BREADCRUMBS = [
     {
@@ -26,11 +28,16 @@ const TaskDetailPage = () => {
 
   const { taskDetail, isLoading } = useTaskGetDetail(id);
 
+  const goBack = () => navigate(-1);
+
   return (
     <div className="p-9">
       <Breadcrumbs breadcrumbs={DETAIL_TASK_BREADCRUMBS} />
 
       {isLoading ? <SpinnerIcon /> : <TaskDetail {...taskDetail} />}
+      <div className="flex justify-end mt-5">
+        <Button onClick={goBack}>Back to tasks</Button>
+      </div>
     </div>
   );
 };
